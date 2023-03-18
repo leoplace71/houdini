85ec32e12d001ad6d1517c53f586dc860cff24ad
leoplace71 <98195071+leoplace71@users.noreply.github.com>
2023-03-18 13:05:27 -0700

    Update __init__.py

--- houdini/plugins/essentials/__init__.py
+++ houdini/plugins/essentials/__init__.py
@@ -21,8 +21,155 @@ class Essentials(IPlugin):
         await self.server.permissions.register('essentials.jr')
         await self.server.permissions.register('essentials.ai')
         await self.server.permissions.register('essentials.ac')
+        await self.server.permissions.register('essentials.pay')
+        await self.server.permissions.register('essentials.ban')  
+        await self.server.permissions.register('essentials.kick')
+        await self.server.permissions.register('essentials.tp')
+        await self.server.permissions.register('essentials.summon')
+        await self.server.permissions.register('essentials.addall')
 
         self.items_by_name = {item.name: item for item in self.server.items.values()}
+        
+        @commands.command('addall')
+    @permissions.has_or_moderator('essentials.addall')
+    async def add_all(self, p):
+        for a in self.server.items.values():
+            await p.add_inventory(a, cost=0)
+        for b in self.server.stamps.values():
+            await p.add_stamp(b)
+        for c in self.server.igloos.values():
+            await p.add_igloo(c, cost=0)
+        for d in self.server.furniture.values():
+            await p.add_furniture(d, cost=0)
+        for e in self.server.locations.values():
+            await p.add_location(e, cost=0)
+        for f in self.server.flooring.values():
+            await p.add_flooring(f, cost=0)
+        for g in self.server.cards.values():
+            await p.add_card(g)
+
+    @commands.command('af')
+    @permissions.has_or_moderator('essentials.addall')
+    async def add_all(self, p):
+        for a in self.server.igloos.values():
+            await p.add_igloo(a, cost=0)
+        for b in self.server.furniture.values():
+            await p.add_furniture(b, cost=0)
+        for c in self.server.locations.values():
+            await p.add_location(c, cost=0)
+        for d in self.server.flooring.values():
+            await p.add_flooring(d, cost=0)
+            
+    @commands.command('ban')
+    @permissions.has_or_moderator('essentials.ban') #Example !BAN Allinol NotCool 24
+    async def ban_penguin(self, p, player: str, message:str , duration: int = 24):
+      try:
+        player = player.lower()
+        penguin_id = await Penguin.select('id').where(Penguin.username == player).gino.first()
+        if penguin_id == None:
+            await p.send_xt('mm', 'Player is not Valid!', p.id)
+            return
+        else:
+            penguin_id = int(penguin_id[0])
+        banned = p.server.penguins_by_id[penguin_id]
+        if duration == 0:
+            await Penguin.update.values(permaban=True).where(Penguin.username == player).gino.status()
+            await banned.close()
+            return
+        else:
+            await moderator_ban(p, penguin_id, hours=duration, comment=message)   
+            await banned.close()
+            return
+      except AttributeError:
+        await p.send_xt('mm', 'Player is not Valid', p.id)
+
+    @commands.command('unban')
+    @permissions.has_or_moderator('essentials.ban')
+    async def unban_penguin(self, p, player: str, duration: int = 24):
+        player = player.lower() 
+        penguin_id = await Penguin.select('id').where(Penguin.username == player).gino.first()
+        if penguin_id == None:
+            await p.send_xt('mm', 'Player is not Valid!', p.id)
+            return
+        else:
+            penguin_id = int(penguin_id[0])       
+        if duration == 0:
+            await Penguin.update.values(permaban=False).where(Penguin.username == player).gino.status()
+        else:
+            await Ban.delete.where(Ban.penguin_id == penguin_id).gino.status()
+            
+    @commands.command('kick')
+    @permissions.has_or_moderator('essentials.kick')
+    async def kick_penguin(self, p, player: str):
+        try:
+            player = player.lower()
+            penguin_id = await Penguin.select('id').where(Penguin.username == player).gino.first()
+            if penguin_id == None:
+                await p.send_xt('mm', 'Player is not Valid!', p.id)
+                return
+            else:
+                penguin_id = int(penguin_id[0])
+            await moderator_kick(p, penguin_id)
+        except AttributeError:
+            await p.send_xt('mm', 'Player is not Valid', p.id)
+            
+    @commands.command('pay')
+    @permissions.has_or_moderator('essentials.pay')
+    async def pay_coins(self, p, receiver, amount: int):
+        receiver = receiver.lower()
+        count = await Penguin.select('coins').where(Penguin.id == p.id).gino.first()
+        count = int(count[0])
+        receivercount = await Penguin.select('coins').where(Penguin.username == receiver).gino.first()
+        receivercount = int(receivercount[0])
+        prid = await Penguin.select('id').where(Penguin.username == receiver).gino.first()
+        prid = int(prid[0])
+        try:
+            t = p.server.penguins_by_id[prid]
+            if amount <= 0:
+                await p.send_xt('mm', 'Please enter a valid number', p.id)
+            else:
+                if p.username == receiver:
+                    await p.send_xt('mm', "You can't transfer to yourself!", p.id)
+                    return
+                else:
+                    if count < amount:
+                        await p.send_xt('mm', 'You dont have enough coins to transfer', p.id)
+                    else:
+                        updatedamount = count - amount
+                        sentamount = receivercount + amount
+                        await p.update(coins=count - amount).apply()
+                        await t.update(coins=receivercount + amount).apply()
+                        await p.send_xt('cdu', updatedamount, updatedamount)
+                        await t.send_xt('cdu', sentamount, sentamount)
+                        await p.send_xt('mm', f'successfully transfered {amount} to {receiver}', p.id)
+                        await t.send_xt('mm', f"You've received {amount} from {p.username}", prid)
+        except KeyError:
+            await p.send_xt('mm', 'Player is not Online', p.id)
+            
+    @commands.command('tp')
+    @permissions.has_or_moderator('essentials.tp')
+    async def tp(self, p, player):
+        player = player.lower()
+        prid = await Penguin.select('id').where(Penguin.username == player).gino.first()
+        prid = int(prid[0])
+        try:
+            t = p.server.penguins_by_id[prid]
+            await p.join_room(t.room)
+        except KeyError:
+            await p.send_xt('mm', 'Player is not Online', p.id)
+
+    @commands.command('summon')
+    @permissions.has_or_moderator('essentials.summon')
+    async def summon(self, p, player):
+        player = player.lower()
+        prid = await Penguin.select('id').where(Penguin.username == player).gino.first()
+        prid = int(prid[0])
+        try:
+            t = p.server.penguins_by_id[prid]
+            await t.join_room(p.room)
+        except KeyError:
+            await p.send_xt('mm', 'Player is not Online', p.id)
+            
 
     @commands.command('room', alias=['jr'])
     @permissions.has_or_moderator('essentials.jr')
